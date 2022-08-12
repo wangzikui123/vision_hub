93646406ab9d92d423d6ed09e5af89d1c94c4eb7
yichen14 <yichen14@illinois.edu>
2022-08-11 21:41:24 -0500

    merging ritm

--- vision_hub/interactive_seg/ritm.py
+++ vision_hub/interactive_seg/ritm.py
@@ -1,3 +1,4 @@
+from pydoc import cli
 import sys
 import os
 import numpy as np
@@ -15,9 +16,11 @@ ritm_dir = os.path.join(os.path.dirname(__file__), 'ritm_interactive_segmentatio
 sys.path.insert(0, ritm_dir)
 
 from isegm.inference.predictors import get_predictor
+from isegm.inference.clicker import Clicker
 from isegm.utils import vis, exp
 from isegm.inference import utils as ritm_utils
 from isegm.inference.evaluation import evaluate_sample, infer_sample_no_gt
+from isegm.inference.clicker import Click
 
 class ritm_segmenter:
     def __init__(self, ritm_dir=ritm_dir, PROB_THRESH=0.49):
@@ -33,6 +36,11 @@ class ritm_segmenter:
         # Possible choices: 'NoBRS', 'f-BRS-A', 'f-BRS-B', 'f-BRS-C', 'RGB-BRS', 'DistMap-BRS'
         brs_mode = 'f-BRS-B'
         self.predictor = get_predictor(model, brs_mode, device, prob_thresh=self.PROB_THRESH)
+
+        self.annotating_image = None
+        # self.predictor.set_input_image(annotating_image)
+        self.clicks_list = []
+        self.pred_mask = None
     
     def auto_eval(self, img, mask, max_clicks=20, iou_thresh=0.85):
         clicks_list, ious_arr, pred = evaluate_sample(img, mask, self.predictor, 
@@ -51,4 +59,17 @@ class ritm_segmenter:
         # ), axis=1)
         
         return pred_mask, num_clicks
+
+    def get_prediction_from_click(self, is_positive, coords):
+        
+        click = Click(is_positive=is_positive, coords=coords)
+        self.clicks_list.append(click)
+        clicker = Clicker(init_clicks=[click])
+        with torch.no_grad():
+            pred_probs = self.predictor.get_prediction(clicker)
+            pred_mask = pred_probs > self.PROB_THRESH
+        ious_list = [] 
+        
+        draw = vis.draw_with_blend_and_clicks(self.annotating_image, mask=pred_mask, clicks_list=self.clicks_list)
+        return draw
 
